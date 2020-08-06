7ea236c865efdd5a52dba892e0de7b38e9d7e713
ACtangle <674253854@qq.com>
2020-08-06 12:23:16 +0800

    登录登出功能完成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,29 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = [{ path: '/login', component: Login }]
+const routes: Array<RouteConfig> = [
+  { path: '/', redirect: '/login' },
+  { path: '/home', component: Home },
+  { path: '/login', component: Login },
+]
 
 const router = new VueRouter({
   routes,
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to: 即将访问
+  // from: 从哪个路径跳转而来
+  // next: 函数，代表放行
+  if (to.path === '/login') return next()
+  const token = window.sessionStorage.getItem('token')
+  if (!token) return next('/login')
+  next()
 })
 
 export default router
