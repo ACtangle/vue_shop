cca30b9f3c7f6eb8833aea9812b9e4e701839877
ACtangle <674253854@qq.com>
2020-08-07 23:22:13 +0800

    首页侧边栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,139 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <!-- 主页区域 -->
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="tiggerCollapse">|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <!-- 一级菜单模板 -->
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="subItem.id + ''"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      // 一级菜单图标
+      iconsObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao',
+      },
+      // 是否折叠
+      isCollapse: false,
+    }
   },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
+    // 获取所有菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get('/menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.message)
+      this.menulist = res.data
+    },
+    // 点击按钮切换菜单折叠与展开
+    tiggerCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+  },
+  created() {
+    this.getMenuList()
   },
 }
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-container {
+  background-color: #373d41;
+}
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 0;
+  font-size: 20px;
+  color: #fff;
+  > div {
+    display: flex;
+    align-items: center;
+    > span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: 0;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.4em;
+  cursor: pointer;
+}
 </style>
