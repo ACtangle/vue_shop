36f9499ef8e5469924c64ee28d877291e7ab5b4a
ACtangle <674253854@qq.com>
2020-08-31 00:14:42 +0800

    添加NPgross响应接口,修改babel配置即添加在生产环境下删除控制台的插件

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,23 @@ import VueQuillEditor from 'vue-quill-editor'
 // 导入富文本编辑器样式表
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
-import 'quill/dist/quill.bubble.css' // for bubble theme
+import 'quill/dist/quill.bubble.css' // for bubble theme'
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 //配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 //请求拦截器
 axios.interceptors.request.use((config) => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  NProgress.start()
+  return config
+})
+// 响应拦截器
+//请求拦截器
+axios.interceptors.response.use((config) => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
