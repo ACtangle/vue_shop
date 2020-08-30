3b294a532213e37e91a0d00961bc0a4f8645bc9a
ACtangle <674253854@qq.com>
2020-08-30 23:34:13 +0800

    echarts 基本数据图

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import GoodsAdd from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -57,6 +58,10 @@ const routes: Array<RouteConfig> = [
       {
         path: '/orders',
         component: Order,
+      },
+      {
+        path: '/reports',
+        component: Report,
       },
     ],
   },
